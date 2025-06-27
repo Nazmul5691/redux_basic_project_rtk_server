273bb6beb3c75512d3769f1c78996b3ddbb2ddce
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-06-28 02:05:44 +0600

    create-modal-using-shadcn

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,3 +1,4 @@
+import { AddTaskModal } from "@/components/module/tasks/AddTaskModal";
 import TaskCard from "@/components/module/tasks/TasksCard";
 import { selectTasks } from "@/redux/features/tasks/taskSlice";
 import { useAppSelector } from "@/redux/hook";
@@ -13,7 +14,10 @@ export default function Tasks() {
 
     return (
         <div>
-            <h1>This is Tasks component</h1>
+            <div className="flex justify-between my-4 ">
+                <h1>Task</h1>
+                <AddTaskModal />
+            </div>
             <div className="flex flex-col gap-4">
                 {
                     tasks.map((task) => (
