5674523c6ec1059b9c99ac7f10aefc4222e5d3cc
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-06-27 21:48:22 +0600

    handel-dark-mode-using-shadcn

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router";
+import { ModeToggle } from "../mood-toggler";
 
 export default function Navbar() {
     return (
-        <nav className="flex gap-4">
-            <Link to="/">Home</Link>
-            <Link to="/users">Users</Link>
-            <Link to="/tasks">Tasks</Link>
+        <nav className="flex gap-4 justify-between">
+            <div className="flex gap-4">
+                <Link to="/">Home</Link>
+                <Link to="/users">Users</Link>
+                <Link to="/tasks">Tasks</Link>
+            </div>
+            <div>
+                <ModeToggle />
+            </div>
         </nav>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import router from './routes/router.tsx'
+import { ThemeProvider } from './providers/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}>
-        
-    </RouterProvider>
+    <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
+      <RouterProvider router={router}>
+
+      </RouterProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
